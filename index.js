3be63c4fc7d29a47f203426cdd92ce7125198230
dibbodas12 <dibbodas228@gmail.com>
2022-11-09 15:38:47 +0600

    loaded single service from database

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,11 +19,23 @@ async function run(){
 
     try{
         const serviceCollection = client.db('photography').collection('services')
+        app.get('/home',async (req,res)=>{
+         const query = {};
+         const cursor = serviceCollection.find(query);
+         const services = await cursor.limit(3).toArray();
+         res.send(services);
+        })
         app.get('/services',async (req,res)=>{
          const query = {};
          const cursor = serviceCollection.find(query);
          const services = await cursor.toArray();
          res.send(services);
+        })
+        app.get('/services/:id', async (req,res)=>{
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)}
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
         })
     }
     finally{
